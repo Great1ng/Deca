25e0d1e982c8f3822c143ecbdbba55e7343cbbd9
great1ng <warriorkesh4@gmail.com>
2022-06-20 15:00:19 +0300

    Add main function

---
+++ cmake/SFMLOptions.cmake
@@ -0,0 +1,9 @@
+# Build options
+
+set(SFML_BUILD_EXAMPLES FALSE)
+set(SFML_BUILD_DOC FALSE)
+
+# Include directories
+
+include_directories(/usr/include/freetype2)
+include_directories(SFML/include)
